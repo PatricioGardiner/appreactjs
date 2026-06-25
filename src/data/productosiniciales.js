a592d477e32004147890bdf58eb836b166e72d70
Patricio Gardiner <patricio.marcelo5@gmail.com>
2026-06-25 13:56:57 -0300

    Add initial product data to productosIniciales.js

---
+++ src/data/productosiniciales.js
@@ -0,0 +1,25 @@
+export const productosIniciales = [
+  {
+    nombre: "1984",
+    autor: "George Orwell",
+    precio: 12000,
+    descripcion:
+      "Novela distópica que explora temas de control social y manipulación de la verdad.",
+    categoria: "Ficción",
+    imagen: "/img/Orwell-1984.jpg", 
+    stock: 10,
+  },
+  
+  
+
+  {
+    nombre: "Don Quijote de la Mancha",
+    autor: "Miguel de Cervantes",
+    precio: 20000,
+    descripcion:
+      "Obra fundamental de la literatura española, protagonizada por Don Quijote y Sancho Panza.",
+    categoria: "Literatura clásica",
+    imagen: "/img/Cervantes-Don-Quijote de la Mancha.jpg",
+    stock: 10,
+  },
+];
